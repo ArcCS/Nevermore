fd3d7bdae1915745d5247301611be72261970869
Arc <nexusarcadia@gmail.com>
2023-05-06 17:36:24 -0400

    fix the gold from pawn.

--- cmd/script_pawn_sell.go
+++ cmd/script_pawn_sell.go
@@ -45,8 +45,8 @@ func (sell) process(s *state) {
 			finalValue = int((.10 * float64(s.actor.GetStat("int"))) * float64(target.Value))
 		} else if s.actor.GetStat("int") >= 10 {
 			finalValue = int((.5 * float64(target.Value)) +
-				(45 / float64(s.actor.Int.Current) * (.25 * float64(target.Value))) +
-				float64(utils.Roll(10, 1, 0)/10)*(.25*float64(target.Value)))
+				((float64(s.actor.Int.Current) / 45) * (.25 * float64(target.Value))) +
+				(float64(utils.Roll(10, 1, 0))/float64(10))*(.25*float64(target.Value)))
 		}
 
 		s.msg.Actor.SendGood("The pawn broker offers you ", strconv.Itoa(finalValue), " for ", target.Name, ".")
