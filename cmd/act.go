82b97be6042114bb8f0322bb43cd307868025500
Solmur <99982878+ilefil@users.noreply.github.com>
2023-05-17 16:40:35 +0300

    Solmur (#51)
    * Fix experience, remove lastactmap. (#47)
    
    Co-authored-by: Arc <nexusarcadia@gmail.com>
    
    * meld update (#49)
    
    adding meld into self check and error message
    
    * threat for lethal and spelling error (#46)
    
    * spelling error
    
    * added 1 threat for lethal blow
    
    Its rare but if your first hit lethals you won't get xp
    
    * Changed flags
    
    * Update combat_kill.go
    
    Change damage to mob stam.
    
    ---------
    
    Co-authored-by: ArcCS <breanna.stryker@gmail.com>
    
    * emote text update
    
    fixing typos and updating new acts
    
    * adding processing bodies
    
    ---------
    
    Co-authored-by: ArcCS <breanna.stryker@gmail.com>
    Co-authored-by: Arc <nexusarcadia@gmail.com>
    Co-authored-by: Timothy-Watrous <92267745+Timothy-Watrous@users.noreply.github.com>

--- cmd/act.go
+++ cmd/act.go
@@ -1,16 +1,17 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/ArcCS/Nevermore/config"
 	"github.com/ArcCS/Nevermore/permissions"
-	"strings"
 )
 
 func init() {
 	addHandler(act{},
 		"Usage:  act performs for all to see \n \n Perform actions.",
 		permissions.Player,
-		"act", "blink", "burp", "cackle", "clap", "confused", "cough", "crossarms", "crossfingers", "dance", "emote", "frown", "grin", "hiccup", "jump", "laugh", "nod", "shake", "shrug", "sneeze", "snap", "smile", "smirk", "spit", "tap", "thumbsdown", "thumbsup", "whistle")
+		"act", "blink", "blush", "bow", "burp", "cackle", "cheer", "chuckle", "clap", "confused", "cough", "crossarms", "crossfingers", "cry", "dance", "emote", "flex", "flinch", "frown", "gasp", "giggle", "grin", "groan", "hiccup", "hug", "jump", "kneel", "laugh", "nod", "ponder", "salute", "shake", "shiver", "shrug", "sigh", "sneeze", "snap", "smile", "smirk", "spit", "stare", "stretch", "tap", "thumbsdown", "thumbsup", "wave", "whistle", "wink", "yawn")
 }
 
 type act cmd
@@ -26,12 +27,22 @@ func (act) process(s *state) {
 			return
 		}
 		action = strings.Join(s.input, " ")
+	} else if cmdStr == "act" {
+		action = "acts out a scene."
 	} else if cmdStr == "blink" {
 		action = "blinks slowly."
+	} else if cmdStr == "blush" {
+		action = "blushes."
+	} else if cmdStr == "bow" {
+		action = "bows respectfully."
 	} else if cmdStr == "burp" {
 		action = "burps."
 	} else if cmdStr == "cackle" {
 		action = "cackles maniacally!"
+	} else if cmdStr == "cheer" {
+		action = "cheers loudly!"
+	} else if cmdStr == "chuckle" {
+		action = "chuckles softly."
 	} else if cmdStr == "clap" {
 		action = "claps " + config.TextPosPronoun[s.actor.Gender] + " hands."
 	} else if cmdStr == "confused" {
@@ -42,42 +53,50 @@ func (act) process(s *state) {
 		action = "crosses " + config.TextPosPronoun[s.actor.Gender] + " arms."
 	} else if cmdStr == "crossfingers" {
 		action = "crosses " + config.TextPosPronoun[s.actor.Gender] + " fingers."
+	} else if cmdStr == "cry" {
+		action = "starts crying."
 	} else if cmdStr == "dance" {
 		action = "does a little jig."
+	} else if cmdStr == "flex" {
+		action = "flexes " + config.TextPosPronoun[s.actor.Gender] + " muscles."
+	} else if cmdStr == "flinch" {
+		action = "flinches."
 	} else if cmdStr == "frown" {
 		action = "frowns."
+	} else if cmdStr == "gasp" {
+		action = "gasps in surprise."
+	} else if cmdStr == "giggle" {
+		action = "giggles."
 	} else if cmdStr == "grin" {
 		action = "grins widely."
+	} else if cmdStr == "groan" {
+		action = "groans."
 	} else if cmdStr == "hiccup" {
 		action = "hiccups!"
+	} else if cmdStr == "hug" {
+		action = "gives a big hug."
 	} else if cmdStr == "jump" {
 		action = "jumps up and down."
+	} else if cmdStr == "kneel" {
+		action = "kneels down."
 	} else if cmdStr == "laugh" {
 		action = "laughs."
 	} else if cmdStr == "nod" {
 		action = "nods."
+	} else if cmdStr == "ponder" {
+		action = "ponders for a moment."
+	} else if cmdStr == "salute" {
+		action = "salutes."
 	} else if cmdStr == "shake" {
 		action = "shakes " + config.TextPosPronoun[s.actor.Gender] + " head."
+	} else if cmdStr == "shiver" {
+		action = "shivers."
 	} else if cmdStr == "shrug" {
 		action = "shrugs " + config.TextPosPronoun[s.actor.Gender] + " shoulders."
-	} else if cmdStr == "smile" {
-		action = "smiles broadly."
-	} else if cmdStr == "smirk" {
-		action = "smirks."
-	} else if cmdStr == "snap" {
-		action = "snaps " + config.TextPosPronoun[s.actor.Gender] + " fingers."
+	} else if cmdStr == "sigh" {
+		action = "sighs."
 	} else if cmdStr == "sneeze" {
-		action = "sneezes! ACHOO!"
-	} else if cmdStr == "spit" {
-		action = "spits on the ground."
-	} else if cmdStr == "tap" {
-		action = "taps" + config.TextPosPronoun[s.actor.Gender] + " foot."
-	} else if cmdStr == "thumbsdown" {
-		action = "gives a thumbs down!"
-	} else if cmdStr == "thumbsup" {
-		action = "gives a thumbs up!"
-	} else if cmdStr == "whistle" {
-		action = "whistles a tune."
+		action = "sneezes!, ACHO"
 	}
 	s.msg.Actor.SendInfo(s.actor.Name, " ", action)
 	s.msg.Observers.SendInfo(s.actor.Name, " ", action)
